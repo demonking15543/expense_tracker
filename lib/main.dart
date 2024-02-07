033c8bada0076fa4e663f2b5a82b2c70403935bc
demonking15543 <demonking15543@gmail.com>
2024-02-07 23:18:47 +0530

    them added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,37 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 189),);
 
 void main() {
+
   runApp(
       MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        // scaffoldBackgroundColor:Color.fromARGB(255, 220, 189, 252)),
+      colorScheme: kColorScheme,
+      appBarTheme: const AppBarTheme().copyWith(
+        backgroundColor: kColorScheme.onPrimaryContainer,
+        foregroundColor: kColorScheme.primaryContainer,
+      ),
+      cardTheme: const CardTheme().copyWith(
+      color:kColorScheme.secondaryContainer,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16, vertical: 8),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kColorScheme.primaryContainer
+        )),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(fontWeight: FontWeight.normal,
+           color: kColorScheme.onSecondaryContainer,
+           fontSize: 14)
+        )
+
+      ),
       home: const  Expenses(),
     ),
   );
