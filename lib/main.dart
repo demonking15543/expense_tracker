1cbc474aaa0ee421e4557f62f490436f47303098
demonking15543 <demonking15543@gmail.com>
2024-02-11 22:07:45 +0530

    dark mode added

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,33 @@ import 'package:flutter/material.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: const Color.fromARGB(255, 96, 59, 189),);
+var kDarkColorScheme=ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125));
 
 void main() {
 
   runApp(
       MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          
+          colorScheme: kDarkColorScheme,
+
+      cardTheme: const CardTheme().copyWith(
+      color:kDarkColorScheme.secondaryContainer,
+      margin: const EdgeInsets.symmetric(
+        horizontal: 16, vertical: 8),
+        ),
+
+        elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: kDarkColorScheme.primaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer
+        )),
+        ),
+
+
+      
       theme: ThemeData().copyWith(
         useMaterial3: true,
         // scaffoldBackgroundColor:Color.fromARGB(255, 220, 189, 252)),
@@ -32,6 +54,7 @@ void main() {
         )
 
       ),
+      themeMode: ThemeMode.system, 
       home: const  Expenses(),
     ),
   );
