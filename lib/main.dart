9caa61f66703b5989e12017005fe11a719d5714d
demonking15543 <demonking15543@gmail.com>
2024-02-11 21:49:16 +0530

    red background added on deleted

--- lib/main.dart
+++ lib/main.dart
@@ -26,9 +26,9 @@ void main() {
           backgroundColor: kColorScheme.primaryContainer
         )),
         textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(fontWeight: FontWeight.normal,
+          titleLarge: TextStyle(fontWeight: FontWeight.bold,
            color: kColorScheme.onSecondaryContainer,
-           fontSize: 14)
+           fontSize: 16)
         )
 
       ),
