058ed9efafcccca2bf0a6c164ba37a3e76d12417
demonking15543 <demonking15543@gmail.com>
2024-02-24 18:45:37 +0530

    responsive size added

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -68,6 +68,9 @@ class _ExpensesState extends State<Expenses>{
  }
   @override
   Widget build(BuildContext context){
+final width = MediaQuery.of(context).size.width;
+
+
     Widget mainContent =const  Center(child: Text("No expense found!. Start adding some"),);
 if(_registeredExpenses.isNotEmpty){
  mainContent = ExpensesList(expenses: _registeredExpenses, onRemoveExpense:_removeExpense ,);
@@ -85,12 +88,20 @@ if(_registeredExpenses.isNotEmpty){
              icon: const Icon(Icons.add))
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
        children:  [
          Chart(expenses: _registeredExpenses),
         Expanded(child: mainContent)
         ], 
-      ),
+      ): Row(
+         children:  [
+         Expanded(child: Chart(expenses: _registeredExpenses)),
+        Expanded(child: mainContent)
+        ],
+
+      )
+      
+      ,
     );
   }
 }
